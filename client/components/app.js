93ed438833e32766e5d850ca7f59bef7b263aba7
Elliot Cheung <elliotccheung@gmail.com>
2017-07-22 14:35:10 -0500

    Set up main component file, added an image file

---
+++ client/components/app.js
@@ -0,0 +1,19 @@
+/**
+The head app page
+*/
+
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Cool website incoming!
+      </div>
+    )
+  }
+}
