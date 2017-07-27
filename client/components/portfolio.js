17e0bbc8b850068848bd1d351a06e54fba3a6ed0
Elliot Cheung <elliotccheung@gmail.com>
2017-07-27 17:03:49 -0500

    Coded in projects

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
 export default class Portfolio extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.projects = [
+    {
+      name: "Stately Shower",
+      image: "statelyshower.png",
+      url: "www.statelyshower.club"
+    },
+    {
+      name: "PartnerUp",
+      image: "partnerup.png",
+      url: "partnerup.makerpass.com"
+    },
+    {
+      name: "Co-Star",
+      image: "costar.png",
+      url: "co-star.herokuapp.com"
+    },
+    {
+      name: "This Website!",
+      image: "personalwebsite.png",
+      url: "www.elliotccheung.com"
+    }]
+  }
+
+  render() {
+
+  }
 }
